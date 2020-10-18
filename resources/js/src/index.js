c7389c646af61be3ad5e57a9ac45fe45a0f1523e
Lennox <omondilenox@outlook.com>
2020-10-18 08:42:12 +0300

    added react index.js

---
+++ resources/js/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {persistor, store} from './redux/store'
+import {PersistGate} from 'redux-persist/integration/react'
+ReactDOM.render(
+  <React.StrictMode>
+      <BrowserRouter>
+          <Provider store={store}>
+              <PersistGate loading={null} persistor={persistor}>
+                  <App />
+              </PersistGate>
+
+          </Provider>
+      </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
