8b1850768157e97bdd058402d3c308ad043a1d99
Lennox <omondilenox@outlook.com>
2020-10-18 08:51:26 +0300

    added base template

---
+++ resources/js/src/components/template/front/Base.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HeaderTemplate from "./header/header";
+const BaseTemplate =()=>{
+
+    return(
+        <div>
+            <HeaderTemplate/>
+        </div>
+    )
+}
+export default BaseTemplate
